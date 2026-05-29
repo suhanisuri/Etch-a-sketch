9c8ab5b0f02416ac98b21255992c7d0131b12bc8
Suhani Suri <suhanisuriwork@gmail.com>
2026-05-29 15:46:30 +0530

    Base ready

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const container=document.querySelector("#container");
 
 const input=document.getElementById("userinput");
-let size;
+let num;
+function createGrid(size){
+    container.innerHTML="";
+    const squareSize=700/size;
+    for(let i=0;i<size*size;i++){
+        const square=document.createElement("div");
+        square.classList.add("square");
+        square.style.width=`${squareSize}px`;
+        square.style.height=`${squareSize}px`;
+
+        square.addEventListener("mouseenter",()=>{
+            square.style.backgroundColor="black";
+    });
+    container.appendChild(square);
+}}
+
 input.addEventListener("keydown",function(event){
     if(event.key==="Enter"){
-        size=input.value;
-        const squareSize=700/size;
-
-    for(let i=0;i<size*size;i++){
-    const square=document.createElement("div");
-    square.classList.add("square");
-    square.style.width=`${squareSize}px`;
-    square.style.height=`${squareSize}px`;
-    container.appendChild(square);
-}
-}
+        if(input.value<=100){
+            num=input.value;
+            createGrid(num);
+        }
+        else{
+            alert("Enter size between 1 to 100 !");
+        }
+    }
 });
+
